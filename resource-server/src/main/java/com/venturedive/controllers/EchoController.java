e840683029c8100d29e7568c06273e82b932c34d
mehdi@venturedive.com <mehdi@venturedive.com>
2019-03-13 13:57:18 +0500

    Showing decoded details in api request

--- resource-server/src/main/java/com/venturedive/controllers/EchoController.java
+++ resource-server/src/main/java/com/venturedive/controllers/EchoController.java
@@ -2,6 +2,7 @@ package com.venturedive.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class EchoController {
 	@GetMapping
 	@PreAuthorize("hasRole('ROLE_CUSTOMER')")
 	public String index(Authentication auth) {
-		return "Greetings from Spring Boot!";
+		OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
+		return new StringBuffer(auth.toString()).append(" -- Decoded details: ").append(details.getDecodedDetails()).toString();
 	}
 }
